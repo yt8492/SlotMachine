822d13723bad19d5841e629de9e789c5c4c2fe49
yt8492 <tmymyut2000@gmail.com>
2018-10-07 01:08:26 +0900

    bug fix

--- app/src/main/java/com/yt8492/slotmachine/MainActivity.kt
+++ app/src/main/java/com/yt8492/slotmachine/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import com.google.android.things.contrib.driver.ht16k33.Ht16k33
 import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
+import java.nio.BufferUnderflowException
 import java.util.*
 
 /**
@@ -30,15 +31,17 @@ import java.util.*
  */
 class MainActivity : Activity(), Runnable {
 
-    val segment = RainbowHat.openDisplay()
-    val buttonA = RainbowHat.openButtonA()
-    val buttonB = RainbowHat.openButtonB()
-    val buttonC = RainbowHat.openButtonC()
-    val ledStrip = RainbowHat.openLedStrip()
-    var slotRunning = false
-    var thread: Thread? = Thread(this)
-    var slotArray = arrayOf(Pair(false, 0), Pair(false, 0), Pair(false, 0), Pair(false, 0))
-    var touchCnt = 0
+    private val segment = RainbowHat.openDisplay()
+    private val buttonA = RainbowHat.openButtonA()
+    private val buttonB = RainbowHat.openButtonB()
+    private val buttonC = RainbowHat.openButtonC()
+    private val ledStrip = RainbowHat.openLedStrip()
+    private var slotRunning = false
+    private var canTouch =true
+    private var canWrite = true
+    private var thread: Thread? = Thread(this)
+    private var slotArray = arrayOf(Pair(false, 0), Pair(false, 0), Pair(false, 0), Pair(false, 0))
+    private var touchCnt = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,7 +63,7 @@ class MainActivity : Activity(), Runnable {
         }
         buttonB.setOnButtonEventListener { _, _ ->
             log("touch B")
-            if (slotRunning) {
+            if (slotRunning && canTouch) {
                 for (i in slotArray.indices) {
                     if (slotArray[i].first) {
                         slotArray[i] = Pair(false, Random().nextInt(10))
@@ -75,7 +78,8 @@ class MainActivity : Activity(), Runnable {
         buttonA.setOnButtonEventListener { _, _ ->
             log("touch A")
             touchCnt++
-            if (slotRunning && touchCnt % 2 == 1) {
+            if (slotRunning && touchCnt % 2 == 1 && canTouch) {
+                canTouch = false
                 for (i in slotArray.indices) {
                     if (slotArray[i].first) {
                         slotArray[i] = Pair(false, slotArray[i].second)
@@ -88,13 +92,22 @@ class MainActivity : Activity(), Runnable {
                         break
                     }
                 }
+                canTouch = true
             }
         }
     }
 
     private fun setSegmentValue() {
-        val displayValue = buildString { append(slotArray[0].second, slotArray[1].second, slotArray[2].second, slotArray[3].second) }
-        segment.display(displayValue)
+        if (canWrite) {
+            canWrite = false
+            try {
+                val displayValue = buildString { append(slotArray[0].second, slotArray[1].second, slotArray[2].second, slotArray[3].second) }
+                segment.display(displayValue)
+            } catch (e: BufferUnderflowException) {
+                e.printStackTrace()
+            }
+            canWrite = true
+        }
     }
 
     private fun checkSlotResult() {
@@ -102,7 +115,7 @@ class MainActivity : Activity(), Runnable {
                 slotArray[1] ==slotArray[2] &&
                  slotArray[2] == slotArray[3]) {
             ledStrip.brightness = 1
-            var rainbow = IntArray(RainbowHat.LEDSTRIP_LENGTH)
+            val rainbow = IntArray(RainbowHat.LEDSTRIP_LENGTH)
             for (i in rainbow.indices) {
                 rainbow[i] = Color.HSVToColor(127, floatArrayOf(i * 360f / rainbow.size, 1f, 1f))
             }
@@ -124,13 +137,16 @@ class MainActivity : Activity(), Runnable {
                 log("Thread")
                 setSegmentValue()
             }
-            Thread.sleep(100)
+            Thread.sleep(125)
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         segment.close()
+        buttonA.close()
+        buttonB.close()
+        buttonC.close()
         ledStrip.close()
         thread = null
     }
